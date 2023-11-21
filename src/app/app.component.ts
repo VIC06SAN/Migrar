3ce44600ca8502d72d44ac0b389c6bbb3235548a
VIC06SAN <vese9876@gmail.com>
2023-11-21 11:26:09 -0500

    init repository dev

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from "./components/login/login.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [
+      CommonModule, 
+      RouterOutlet, 
+      LoginComponent
+    ]
 })
 export class AppComponent {
   title = 'app-academico-front';
